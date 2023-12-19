c8a08dbe0719e65c2f28680e4c59c8db51db0a1c
Yupl <chursin_a@garpix.com>
2023-12-19 19:40:39 +0300

    sprint_1 init

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import Handlebars from 'handlebars'
+
+import * as Pages from './pages'
+import * as Components from './components'
+
+const pages = {
+  'login': [ Pages.LoginPage, {test: '123'} ]
+};
+
+Object.entries(Components).forEach(([ name, components ]) => {
+  Handlebars.registerPartial(name, components)
+})
+
+function navigate(page: string) {
+  //@ts-ignore
+  const [ source, context ] = pages[page];
+  const container = document.getElementById('app')!;
+  container.innerHTML = Handlebars.compile(source)(context);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+document.addEventListener('click', e => {
+  //@ts-ignore
+  const page = e.target.getAttribute('page');
+  if (page) {
+    navigate(page);
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+});
