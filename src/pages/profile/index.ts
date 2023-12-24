367915475eb570746d23c102f87168813657c779
maBhaby <artem_chursin_37@mail.ru>
2023-12-24 23:42:59 +0300

    sprint_1 nemnogo pravok

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -3,12 +3,8 @@ import Handlebars from 'handlebars'
 export { default as ProfilePage } from './profile.hbs?raw'
 
 import { USER_VALUE } from './consts'
-
-Handlebars.registerHelper('profileValue', (ctx, options) => {
-  console.log('ctx', ctx);
-  console.log('options', options, this);
-  
-  
+// TODO - вынести хелпер
+Handlebars.registerHelper('profileValue', () => {
   return USER_VALUE
 })
 
